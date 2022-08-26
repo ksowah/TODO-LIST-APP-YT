de8757957d29414ecda2de6a75854d5eefbb05f9
Davy Engone <davy@hackages.io>
2022-08-26 16:46:29 +0200

    format code and installed eslint

--- src/App.js
+++ src/App.js
@@ -1,97 +1,94 @@
-import "./App.css"
-import { TbArrowBarDown } from 'react-icons/tb';
+import React from 'react'
+import "./App.css";
+import { TbArrowBarDown } from "react-icons/tb";
 import ListItem from "./components/ListItem";
 import { useEffect, useState } from "react";
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [allTodos, setAllTodos] = useState([]);
 
-    const [todo, setTodo] = useState("")
-    const [allTodos, setAllTodos] = useState([])
+  const addTodo = (e) => {
+    e.preventDefault();
 
-    const addTodo = (e) => {
-        e.preventDefault()
+    const todoItem = {
+      id: new Date().getTime(),
+      text: todo,
+      isChecked: false,
+    };
 
-        const todoItem = {
-            id: new Date().getTime(),
-            text: todo,
-            isChecked: false
-        }
-
-        if(todo !== ""){
-            setAllTodos([...allTodos].concat(todoItem).reverse())
-            setTodo("")
-        }
-
-        console.log(allTodos)
+    if (todo !== "") {
+      setAllTodos([...allTodos].concat(todoItem).reverse());
+      setTodo("");
     }
+  };
 
-    
+  const getAllTodos = () => {
+    let stored = JSON.parse(localStorage.getItem("todo"));
 
-    const getAllTodos = () => {
-        let stored = JSON.parse(localStorage.getItem("todo"))
-
-        if(stored) {
-            setAllTodos(stored)
-        }
+    if (stored) {
+      setAllTodos(stored);
     }
+  };
 
+  const toggleChecked = (id) => {
+    let updatedTodos = [...allTodos].map((todo) => {
+      if (todo.id === id) {
+        todo.isChecked = !todo.isChecked;
+      }
 
-    const toggleChecked = (id) => {
-        let updatedTodos = [...allTodos].map(todo => {
-            if(todo.id === id){
-                todo.isChecked = !todo.isChecked
-            }
+      return todo;
+    });
 
-            return todo
-        })
+    setAllTodos(updatedTodos);
+  };
 
-        setAllTodos(updatedTodos)
-    }
+  const deleteTodo = (id) => {
+    const filteredTodo = allTodos.filter((todo) => todo.id !== id);
+    setAllTodos(filteredTodo);
+  };
 
+  useEffect(() => {
+    getAllTodos();
+  }, []);
 
-
-    const deleteTodo = (id) => {
-        const filteredTodo = allTodos.filter(todo => todo.id !== id)
-        setAllTodos(filteredTodo)
-    }
-
-
-    useEffect(() => {
-        getAllTodos()
-    }, [])
-
-
-    useEffect(() => {
-        localStorage.setItem("todo", JSON.stringify(allTodos))
-    }, [allTodos])
-    
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(allTodos));
+  }, [allTodos]);
 
   return (
     <div className="App">
-        <div className="App_todo">
-            <form className="App_input_wrapper" onSubmit={addTodo} >
-                <input type={"text"} className="App_input" value={todo} onChange={(e) => setTodo(e.target.value)} />
-                <div className="App_input_button" onClick={addTodo} >
-                    <TbArrowBarDown size={24} />
-                </div>
-            </form>
+      <div className="App_todo">
+        <form className="App_input_wrapper" onSubmit={addTodo}>
+          <input
+            type={"text"}
+            className="App_input"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <div className="App_input_button" onClick={addTodo}>
+            <TbArrowBarDown size={24} />
+          </div>
+        </form>
 
-            <div className="App_todo_list">
-                {
-                    allTodos.map(todo => (
-                        <ListItem key={todo.id} deleteTodo={() => deleteTodo(todo.id)} text={todo.text} isChecked={todo.isChecked} toggleChecked={() => toggleChecked(todo.id)} />
-                    ))
-                }
+        <div className="App_todo_list">
+          {allTodos.map((todo) => (
+            <ListItem
+              key={todo.id}
+              deleteTodo={() => deleteTodo(todo.id)}
+              text={todo.text}
+              isChecked={todo.isChecked}
+              toggleChecked={() => toggleChecked(todo.id)}
+            />
+          ))}
 
-                {
-                    allTodos.length === 0 && (
-                        <p className="empty">There are no Todo's</p>
-                    )
-                }
-            </div>
+          {allTodos.length === 0 && (
+            <p className="empty">There are no Todo's</p>
+          )}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,23 +1,26 @@
-import "./listItem.css"
-import { AiOutlineCheck } from 'react-icons/ai';
-import { ImBin } from 'react-icons/im';
+import "./listItem.css";
+import { AiOutlineCheck } from "react-icons/ai";
+import { ImBin } from "react-icons/im";
 
-const ListItem = ({text, deleteTodo, isChecked, toggleChecked}) => {
+const ListItem = ({ text, deleteTodo, isChecked, toggleChecked }) => {
   return (
     <div className="todo">
-        <div className={`${isChecked ? "todo_radio" : "todo_radio_unchecked" }`} onClick={toggleChecked}>
-            {isChecked && <AiOutlineCheck />}
-        </div>
+      <div
+        className={`${isChecked ? "todo_radio" : "todo_radio_unchecked"}`}
+        onClick={toggleChecked}
+      >
+        {isChecked && <AiOutlineCheck />}
+      </div>
 
-        <div className={` ${isChecked ? "todo_text_checked" : "todo_text"}`}>
-            {text}
-        </div>
+      <div className={` ${isChecked ? "todo_text_checked" : "todo_text"}`}>
+        {text}
+      </div>
 
-        <div className="todo_delete" onClick={deleteTodo}>
-            <ImBin />
-        </div>
+      <div className="todo_delete" onClick={deleteTodo}>
+        <ImBin />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListItem
+export default ListItem;
